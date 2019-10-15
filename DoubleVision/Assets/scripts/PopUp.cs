ea8076400672a203add414ecfb387e969effef2d
masha-krotkykh <31903446+masha-krotkykh@users.noreply.github.com>
2019-10-14 23:31:50 -0400

    Game project first commit

---
+++ DoubleVision/Assets/scripts/PopUp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopUp : MonoBehaviour
+{
+    public Text PopUpText;
+
+    public void ShowPopUp(string text)
+    {
+        gameObject.SetActive(true);
+        PopUpText.text = text;
+        StartCoroutine("WaitForSec");
+
+    }
+
+    IEnumerator WaitForSec()
+    {
+      yield return new WaitForSeconds(5);
+      gameObject.SetActive(false);
+    }
+
+    public void HidePopUp()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ DoubleVision/Assets/scripts/navigation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class navigation : MonoBehaviour
+{
+
+   // functions to change locations
+   public void goBack()
+   {
+      SceneManager.LoadScene("room_00_back");
+   }
+
+   public void goFront()
+   {
+     SceneManager.LoadScene("room_00_front");
+   }
+
+   public void goLeft()
+   {
+     SceneManager.LoadScene("room_00_left");
+   }
+
+   // functions to be called when a game object is clicked
+    void OnMouseDown()
+    {
+
+    }
+}
