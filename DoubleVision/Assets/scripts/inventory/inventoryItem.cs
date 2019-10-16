bc2f7a9e7c54996a10843bce5121fc58ecbaba70
masha-krotkykh <31903446+masha-krotkykh@users.noreply.github.com>
2019-10-16 00:55:31 -0400

    Began inventory system

---
+++ DoubleVision/Assets/scripts/inventory/inventoryItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Items")]
+public class inventoryItem : ScriptableObject
+{
+  public string itemName;
+  public string itemDescription;
+  public Sprite itemImage;
+  public bool usable;
+}
---
+++ DoubleVision/Assets/scripts/inventory/inventorySlot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class inventorySlot : MonoBehaviour
+{
+    [Header("UI fields to change")]
+    [SerializeField] private Image itemImage;
+
+    [Header("Values from the item")]
+    public inventoryItem thisItem;
+    public inventoryManager thisManager;
+
+    public void Setup(inventoryItem newItem, inventoryManager newManager)
+    {
+      thisItem = newItem;
+      thisManager = newManager;
+      if(thisItem != null)
+      {
+        itemImage.sprite = thisItem.itemImage;
+      }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
