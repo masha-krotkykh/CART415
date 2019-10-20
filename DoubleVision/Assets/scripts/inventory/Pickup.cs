378cf087442cdac18ced7c38778b9eeeac3cd0f9
masha-krotkykh <31903446+masha-krotkykh@users.noreply.github.com>
2019-10-19 23:37:09 -0400

    got basic inventory functionality to work

---
+++ DoubleVision/Assets/scripts/inventory/Pickup.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pickup : MonoBehaviour
+{
+  public PopUp popup;
+
+  public static string nameOfObject;
+  public static string popupMessage;
+
+  private Inventory inventory;
+  public GameObject itemButton;
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("EventSystem").GetComponent<Inventory>();
+    }
+
+    // on mouse click look through the array of inventory slots to check is each of themis full
+    void OnMouseDown()
+    {
+      for (int i = 0; i < inventory.slots.Length; i++)
+      { // once we find the slot that is not full, we set it to full since we're adding a new item to it
+        if(inventory.isFull[i] == false)
+        {
+           // Item can be added
+           inventory.isFull[i] = true;
+           // make sure that the button spawns at the same position as the inventory slot
+           Instantiate(itemButton, inventory.slots[i].transform, false);
+           // Naming the object by its object name
+           nameOfObject = gameObject.name;
+           Debug.Log("You found " + nameOfObject);
+           //Getting an individual message from gameObject to display in a popup
+           popupMessage = gameObject.GetComponent<Text>().text;
+           popup.ShowPopUp(popupMessage);
+           // Destroy the game object from the scene
+           Destroy(gameObject);
+           // and the loop stops
+           break;
+        }
+      }
+    }
+}
